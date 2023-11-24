84c91f899db2a4ad05035120abf3c41128be9f0b
苡溱 廖 <5b0g0904@office.stust.edu.tw>
2023-11-24 16:42:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/GDSC.kt
@@ -0,0 +1,73 @@
+package com.example.myapplication
+
+fun main(){
+
+
+    var times =0
+
+    for(num in 1..<100 step 5){
+        println(num)
+        //times += 1
+    }
+    /*for(i in 100 downTo 1 ){
+        println(i)
+
+    }*/
+
+/*
+    val score = 70
+
+    var level =""
+
+    when(score){
+        in 90 .. 100 -> level = "甲等"
+        in 80 .. 89 -> level = "乙等"
+        in 70 .. 79 -> level = "丙等"
+        else -> "不及格"
+    }
+    println(level)
+
+
+    val a = 10
+    val b = 20
+
+    if(a == b){
+        println("A 等於 B")
+    }else{
+        println("A 不等於 B")
+
+    }
+
+   val text:String = "晴天"
+   val num:Int = 10
+
+   val output = text is String
+        println(text)
+
+
+   val str = "hello\n world"
+   val  text = 'text' //錯誤
+   val text:String = "晴天"
+   val num:Int = 10
+
+   val isSunny = true
+
+   if(isSunny == true){
+       println("晴天")
+   }else{
+       println("不是晴天")
+   }
+
+   println("Hello world!")
+   val text = "文字"  //不可變
+
+   val number = 10
+   val float = 10.0f //浮點數
+
+   \n  //換行
+   \t  //Tab
+
+    */
+
+}
+
---
+++ app/src/main/java/com/example/myapplication/GDSC3.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+fun main(){
+    val EngineeringCalculator : Calculator(){
+
+    }
+
+}
+//private<protected<internal<
+open class Calculator{
+
+    fun plus () {
+
+    }
+}
